ee5332999db5fde803cb25ebeca38230a3a8195d
chuynh <huynh.cedric@gmail.com>
2014-11-06 11:32:22 +0100

    page événements :affiche la liste de toutes les entreprises et étudiants. RAF : afficher les listes de ceux qui participent et pouvoir éditer les listes.

--- public/public/app/js/controllers/editControllers/editEventController.js
+++ public/public/app/js/controllers/editControllers/editEventController.js
@@ -1,26 +1,35 @@
 'use strict';
 
-app.controller('editEventController', ['$scope','$routeParams','eventResource', function($scope, $routeParams, eventResource){
-    
-    var eventId;
-    
-    $scope.event = {}
-    var init = function() {
-        eventId = $routeParams.eventId;
-        eventResource.getEvent({'id':eventId}, function(data) {
-            console.log(data);
-            
-            $scope.event = data;
-        });
-    }
-    
-    $scope.editAttendingCompanies = function() {
-        console.log("Le bouton fonctionne");
-    }
-    
-    $scope.editAttendingCandidate = function () {
-        console.log("le bouton fonctionne");
-    }
-    
-    init();
-}]);
+app.controller('editEventController', ['$scope', '$routeParams', 'eventResource', 'enterpriseResource', 'studentResource', 
+    function($scope, $routeParams, eventResource, enterpriseResource, studentResource) {
+
+        var eventId;
+
+        $scope.event = {}
+        var init = function() {
+            eventId = $routeParams.eventId;
+            eventResource.getEvent({'id': eventId}, function(data) {
+                console.log(data);
+
+                $scope.event = data;
+            });
+
+            enterpriseResource.listEnterprises(function(data) {
+                $scope.enterprises = data;
+            });
+
+            studentResource.listStudents(function(data) {
+                $scope.students = data;
+            });
+        }
+
+        $scope.editAttendingCompanies = function() {
+            console.log("Le bouton fonctionne");
+        }
+
+        $scope.editAttendingCandidate = function() {
+            console.log("le bouton fonctionne");
+        }
+
+        init();
+    }]);
