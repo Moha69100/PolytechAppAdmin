50e3da29c4d938f8e45fc70b9f8b4bb66175b5ef
David COHEN <david.alex.cohen@gmail.com>
2014-11-07 11:53:27 +0100

    Mise en place ihm planning (temporaire...)

---
+++ public/public/app/js/controllers/test/planningController.js
@@ -0,0 +1,9 @@
+app.controller('planningController', ['$rootScope', '$scope', 
+	function($rootScope, $scope) {
+
+        var init = function () {
+            
+        };
+
+        init();
+}]);
