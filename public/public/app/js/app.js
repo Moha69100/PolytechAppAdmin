46e2b077f433f7427889fe9cef0f98f0219d0e9e
David COHEN <david.alex.cohen@gmail.com>
2014-11-17 11:46:05 +0100

    Correction bug fonts / update menu

--- public/public/app/js/app.js
+++ public/public/app/js/app.js
@@ -73,12 +73,6 @@ app.config(['$routeProvider', function($routeProvider) {
             menu: {
                 id: 'menu-event'
             }
-        }).when('/planning', {
-            controller: 'planningController',
-            templateUrl: 'app/partials/planning/planning.html',
-            menu: {
-                id: 'menu-planning'
-            }
         }).otherwise({
             controller: 'accueilController',
             redirectTo: '',
