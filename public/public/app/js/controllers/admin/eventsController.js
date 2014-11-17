d7bff6f34821a8bc361894febd9e01aaf51a269b
unknown <epulapp@EPULAPP30.univ-lyon1.fr>
2014-11-17 12:01:54 +0100

    commit

--- public/public/app/js/controllers/admin/eventsController.js
+++ public/public/app/js/controllers/admin/eventsController.js
@@ -1,16 +1,19 @@
 app.controller('eventsController', ['$rootScope', '$scope', '$http', '$timeout', '$location', '$window', 'browser', 'eventResource',
-    function ($rootScope, $scope, $http, $timeout, $location, $window, browser, eventResource) {
+    function($rootScope, $scope, $http, $timeout, $location, $window, browser, eventResource) {
+
+        console.log("plop")
 
 
-
-        var init = function () {
-            $scope.events = eventResource.listEvents();
+        var init = function() {
+            eventResource.listEvents(function(data) {
+                $scope.events = data;
+            });
         };
 
         $scope.editEvent = function(event) {
             $location.search('eventId', event.id).path('edit-event');
         }
-        
+
         init();
 
     }]);
