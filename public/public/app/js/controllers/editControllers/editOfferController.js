14244558c342930784a793821e6191928bc77aa0
chuynh <huynh.cedric@gmail.com>
2014-11-18 11:48:24 +0100

    Chargement de l'entreprise provenant de la base dans la liste des entreprises lors d'une édition sur offres

--- public/public/app/js/controllers/editControllers/editOfferController.js
+++ public/public/app/js/controllers/editControllers/editOfferController.js
@@ -1,19 +1,22 @@
 app.controller("editOfferController", ['$scope', 'offerResource', 'enterpriseResource', '$routeParams', '$location',
-    function ($scope, offerResource, enterpriseResource, $routeParams, $location) {
+    function($scope, offerResource, enterpriseResource, $routeParams, $location) {
 
         $scope.offer = [];
 
         $scope.offer.entreprise;
+        $scope.enterprises;
 
-        var init = function () {
+        var init = function() {
             $scope.offerId = $routeParams.offer;
-            offerResource.getOffer({"id": $scope.offerId}, function (data) {
-                $scope.offer = data;
+            enterpriseResource.listEnterprises(function(data) {
+                $scope.enterprises = data;
+                offerResource.getOffer({"id": $scope.offerId}, function(data) {
+                    $scope.offer = data;
+
+                });
             });
 
-            enterpriseResource.listEnterprises(function (data) {
-                $scope.enterprises = data;
-            });
+
 
         };
 
@@ -21,33 +24,33 @@ app.controller("editOfferController", ['$scope', 'offerResource', 'enterpriseRes
         // 'feedback' serveur
         $scope.feedback = null;
 
-        $scope.saveOffer = function (offer) {
-            offerResource.addOffer({}, $scope.offer, function (data) {
+        $scope.saveOffer = function(offer) {
+            offerResource.addOffer({}, $scope.offer, function(data) {
                 console.log(data + "success");
-            }, function (error) {
+            }, function(error) {
                 console.log(error + " error ");
             });
         };
 
-        $scope.updateOffer = function () {
+        $scope.updateOffer = function() {
 
-            offerResource.updateOffer({}, $scope.offer, function (data) {
+            offerResource.updateOffer({}, $scope.offer, function(data) {
                 console.log($scope.offer);
                 console.log(data + "success");
-            }, function (error) {
+            }, function(error) {
                 console.log(error + " error ");
             });
         };
 
-        $scope.removeOffer = function (offer) {
-            offerResource.removeOffer({"id": $scope.offerId}, function (data) {
+        $scope.removeOffer = function(offer) {
+            offerResource.removeOffer({"id": $scope.offerId}, function(data) {
                 $location.path('/admin-offers');
             });
         };
         /**
          * sortie par cancel()
          */
-        $scope.cancel = function () {
+        $scope.cancel = function() {
             delete ($scope.offer);
             delete ($scope.offerId);
             $location.url('/admin-offers');
