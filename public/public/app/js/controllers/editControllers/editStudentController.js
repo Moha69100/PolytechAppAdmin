a21da7ac9a12556a733569bae41a5e085fb3a4d9
Geoffrey <jojo45452@gmail.com>
2014-11-18 11:54:56 +0100

    files

--- public/public/app/js/controllers/editControllers/editStudentController.js
+++ public/public/app/js/controllers/editControllers/editStudentController.js
@@ -4,11 +4,11 @@ app.controller("editStudentController", ['$scope', 'studentResource', '$routePar
         $scope.filePresent = [];
         $scope.fileType = [];
         $scope.fileList = [];
-        $scope.uploader.url = "http://localhost:8090/upload";
-        // if (AuthenticationData.isAuthenticated()) {
-        $scope.uploader.headers.api_key = AuthenticationData.getApiKey();
+        $scope.uploader.url = "http://localhost:8090/upload/student/" + $routeParams.student;
         $scope.uploader.withCredentials = true;
-        //}
+        if (AuthenticationData.isAuthenticated()) {
+            $scope.uploader.headers.api_key = AuthenticationData.getApiKey();
+        }
 
         var init = function () {
             $scope.studentId = $routeParams.student;
@@ -16,31 +16,31 @@ app.controller("editStudentController", ['$scope', 'studentResource', '$routePar
                 $scope.student = data;
                 initFileList();
             });
-
         };
         init();
-
         // 'feedback' serveur
         $scope.feedback = null;
-
         $scope.bacList = [
             {"name": "S"},
             {"name": "L"},
             {"name": "ES"}
         ];
-
         $scope.statCandidature = [
             {"name": "Acceptée"},
             {"name": "En cours"},
             {"name": "Rejetée"}
         ];
-
         $scope.save = function (student) {
             var postData = $scope.student;
             studentResource.updateStudent({}, postData, function () {
+
                 $scope.uploader.uploadAll();
             });
         };
+        $scope.uploader.onBeforeUploadItem = function (item) {
+            item.url = "http://localhost:8090/upload/student/" + $routeParams.student + "/type/" + item.fileType;
+        };
+
 
         $scope.removeStudent = function (student) {
             studentResource.removeStudent({"id": $scope.studentId}, function (data) {
@@ -55,11 +55,10 @@ app.controller("editStudentController", ['$scope', 'studentResource', '$routePar
             delete ($scope.studentId);
             $location.url('/admin-student');
         };
-
         var initFileList = function () {
             $http({
                 method: "GET",
-                url: "http://localhost:8090/upload/student/1"}// + $routeParams.student}
+                url: "http://localhost:8090/upload/student/files/" + $routeParams.student}
             ).success(function (data) {
                 angular.forEach(data, function (file, key) {
                     $scope.fileList.push({
@@ -69,33 +68,20 @@ app.controller("editStudentController", ['$scope', 'studentResource', '$routePar
                     $scope.filePresent[file[1]] = true;
                 });
             });
-            /*    $scope.fileList.push({
-             name: "CV",
-             type: "CV"
-             }, {
-             name: "lettreMotiv",
-             type: "lettreMotiv"
-             });
-             $scope.filePresent["CV"] = true;
-             $scope.filePresent["lettreMotiv"] = true;*/
         };
-
         $scope.removeFileFromQueue = function (item) {
             item.remove();
             $scope.filePresent[item.fileType] = false;
         };
-
         $scope.upload = function () {
             $scope.uploader.uploadAll();
         };
-
         $scope.uploader.onCompleteAll = function () {
             delete ($scope.filePresent);
             $scope.filePresent = [];
             $scope.uploader.clearQueue();
-            $scope.uploader.url = "http://localhost:8090/upload";
+            $scope.uploader.url = "http://localhost:8090/upload/student/" + $routeParams.student;
         };
-
         $scope.openUploadPopup = function (fileTypeHtml) {
             $scope.fileType = fileTypeHtml;
             var modalInstance = $modal.open({
@@ -111,9 +97,9 @@ app.controller("editStudentController", ['$scope', 'studentResource', '$routePar
                     }
                 }
             });
-
             modalInstance.result.then(function (result) {
                 $scope.filePresent[$scope.fileType] = true;
+                console.log($scope.uploader)
             }, function (result) {
                 if ($scope.filePresent[$scope.fileType]) {
                     $scope.filePresent[$scope.fileType] = false;
