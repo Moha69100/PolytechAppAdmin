a448dff450267b27fa52532048555b55dafd03a5
Florian.Courtial <floriancourtial@gmail.com>
2014-11-20 08:48:36 +0100

    Test with the prod backend V2.

--- public/public/app/js/resources/enterpriseResource.js
+++ public/public/app/js/resources/enterpriseResource.js
@@ -5,7 +5,7 @@
  */
 app.factory('enterpriseResource', ['$resource', function ($resource) {
 
-        var BASE_URL = 'http://projet-a5a.univ-lyon1.fr:8090/';
+        var BASE_URL = 'http://projet-a5a.univ-lyon1.fr:8090';
         var FIND_ALL = BASE_URL + '/entreprises';
         var GET_BY_ID = BASE_URL + '/entreprise/:id';
         var ADD_ENTERPRISE = BASE_URL + '/entreprise/add';
