331fdb550534e8628a58b441960c2981dcf4cb34
Epulapp <Epulapp@EPULAPP27.univ-lyon1.fr>
2014-11-20 11:40:48 +0100

    rectification du main()

--- PolytechAppAdminBackEnd/src/main/java/com/polytech/controller/Application.java
+++ PolytechAppAdminBackEnd/src/main/java/com/polytech/controller/Application.java
@@ -18,22 +18,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        String error = "";
-        String id = "2";
-        
-        try {
-            EvenementManager evenementManager = new EvenementManager();
-            //Evenement evt = null;
-            //evt = evenementManager.getEvenementById(Integer.parseInt(id));
-            Evenement evt = evenementManager.getEvenementById(Integer.parseInt(id));
-            PlanningGenerator planning = new PlanningGenerator(evt, 30);
-
-            planning.generate();
-        } catch (Exception ex) {
-            error = ex.getMessage();
-        }
-
     }
 
     @Bean
